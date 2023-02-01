2ef0008659186a1b2fc4bf7fc46f6e10ce1ad927
Ahmed <balochsaeed888@gmail.com>
2023-02-01 13:48:19 +0500

    halfway part 4 .styling nav

---
+++ main.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener("click",()=>{
+
+    // primaryNav.classList.toggle("primary-navigation")
+   primaryNav.hasAttribute("data-visible")?primaryNav.setAttribute("aria-expended",false) :  primaryNav.setAttribute("aria-expended",true)
+  
+
+    primaryNav.toggleAttribute("data-visible")
+})
