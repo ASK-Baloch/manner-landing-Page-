1eb11ceef366fa506ea2ea1773c0dad5cf5d123f
Ahmed <balochsaeed888@gmail.com>
2023-02-04 02:32:24 +0500

    commiting changes .part 10 fixing nav

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-const primaryHeader=document.querySelector('.primary-header')
-const navToggle = document.querySelector('.mobile-nav-toggle');
-const primaryNav = document.querySelector('.primary-navigation');
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
-navToggle.addEventListener("click",()=>{
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
 
-    // primaryNav.classList.toggle("primary-navigation")
-   primaryNav.hasAttribute("data-visible")?primaryNav.setAttribute("aria-expended",false) :  primaryNav.setAttribute("aria-expended",true)
-  
-
-    primaryNav.toggleAttribute("data-visible")
-    primaryHeader.toggleAttribute("data-overlay")
-})
-
- const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: true,
-    dots: true,
-    centerMode:true,
-    responsive:{
-      480:{
-        dots:false,
-      }
-    }
-  });
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
+});
